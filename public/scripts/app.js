04f07d992e509720cd01f7b60d63b9514abf8a64
g g <gabedotgutierrez@gmail.com>
2024-10-07 15:51:52 -0400

    grid on/off working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,25 @@
 console.log("hello world")
 
+/* <option value="black">Black</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="yellow">Yellow</option>
+                <option value="grey">Grey</option>
+                <option value="cyan">Cyan</option>
+                <option value="magenta">Magenta</option>
+                <option value="brown">Brown</option>
+                <option value="white">White</option> */
+
 let currentColor = "black"
 let colorArray = []
-
-// const sampleGrid = ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'green', 'green', 'green', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow']
+let selectColors = ["Black", "Red", "Green", "Blue", "Yellow", "Grey", "Cyan", "Magenta", "Brown", "White"]
+let grid = true
 
 const container = document.querySelector(".flex-container")
 const newContainer = document.querySelector("#new-container")
 const colorSelect = document.querySelector("#color-select")
 const colorInput = document.querySelector("#colorArray")
-// const grid = document.querySelector(".display-grid") 
 let cells
 
 
@@ -18,19 +28,43 @@ function makeGrid() {
         let div = document.createElement("div")
         div.classList.add("flex-item")
         div.setAttribute("id",`cell`+ i)
-        // div.style.backgroundColor = sampleGrid[i]
         newContainer.appendChild(div)
     }
 }
 
-// function makeDisplay(){
-//     for(let i = 0; i < 64; i++){
-//           let div = document.createElement("div")
-//           div.classList.add("flex-item")
-//           div.style.backgroundColor = art.colorArray
-//           grid.appendChild(div)
-//       }
-//     }
+function makeGrid256(){
+    for(let i = 0; i < 256; i++){
+        let div = document.createElement("div")
+        div.classList.add("flex-item-256")
+        div.setAttribute("id",`cell`+ i)
+        newContainer.appendChild(div)
+    }
+}
+
+function gridToggle(){
+    cells = document.querySelectorAll(".flex-item")
+    console.log(cells[0])
+    if (grid){
+        cells.forEach((cell)=>{
+            cell.style.border = "none"
+        })
+        grid = !grid
+    } else {
+        cells.forEach((cell)=>{
+            cell.style.border = "2px dashed black"
+        }) 
+        grid = !grid
+    }
+}
+
+function makeColorSelect(){
+    for(let i =0; i < selectColors.length; i++){
+        let option =  document.createElement("option")
+        option.value = selectColors[i]
+        option.text = selectColors[i]
+        colorSelect.add(option)
+    }
+}
 
 function collectColors() {
     cells = document.querySelectorAll(".flex-item")
@@ -43,7 +77,7 @@ function collectColors() {
 
 function handleClick(e) {
     // console.log(`you've clicked ${e.target.id}`)
-    if  (e.target.className === "flex-item") {
+    if  (e.target.className === "flex-item" || "flex-item-256") {
         
         e.target.style.backgroundColor = currentColor   
     }
@@ -57,9 +91,14 @@ function handleChange(e){
     // console.log(currentColor)
 }
 
+makeGrid()
+// makeGrid256()
+makeColorSelect()
+
 container.addEventListener("click",handleClick)
 colorSelect.addEventListener("change",handleChange)
 
+//adds color array as an hidden input to form
 let form =  document.querySelector('#art-form')
 form.addEventListener("submit",()=>{
     collectColors()
@@ -67,5 +106,3 @@ form.addEventListener("submit",()=>{
 
 })
 
-makeGrid()
-// makeDisplay()
